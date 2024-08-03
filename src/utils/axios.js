379dedcf22b645b2d9fac582cb1bce8f3795d3aa
leylagahramanova <leylagahramanova13@gmail.com>
2024-08-03 16:04:13 +0400

    Initial commit after reinitializing repository

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'task-server-hj49p0tm3-leylagahramanovas-projects.vercel.app/api',
+    baseURL: 'https://task-server-mqvfko2uw-leylagahramanovas-projects.vercel.app/api',
 });
 
 instance.interceptors.request.use(
